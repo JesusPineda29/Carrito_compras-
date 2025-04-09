1920f3963c1d0fadfe8f6acc8fcc83a834a3ff52
JesusPineda29 <jesuspineda1429@gmail.com>
2025-04-09 15:58:59 -0600

    agrega carrito de compras interactivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
 import { useState } from "react"
+import { useEffect } from "react"
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
 import { Guitar } from "./components/Guitar"
+import { db } from "./data/db"
 
 
 function App() { 
 
-    // State hoocks
-    const [auth, setAuth] = useState([])   
-    const [total, setTotal] = useState(0)
-    const [cart, setCart] = useState([])
+
+  const [data, setData] = useState(db)
+  const [cart, setCart] = useState([])
+  
+
+  function addToCart(item) {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id)
+    if(itemExist>=0) {
+      const updatedCart = [...cart]
+      updatedCart[itemExist].quantity++
+      setCart(updatedCart)
+    }else{
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+  }
 
 
   return (
     <>
-    <Header/>
+    <Header
+      cart={cart}
+    />
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar/>
+            {data.map((guitar)=>(
+                <Guitar
+                  key={guitar.id}
+                  guitar={guitar}
+                  setCart={setCart}
+                  addToCart={addToCart}
+                
+                />                
+            ))}
             
-
+        
         </div>
     </main>
 
